e58b1868689cf3fc95d8aa6072c2dab5ffe4fba8
Anderson Rodrigues <anderson.erdeval@gmail.com>
2022-01-02 14:18:36 -0400

    commit inicial

---
+++ JavaScript/js/index.js
@@ -0,0 +1,79 @@
+/*AULA 152
+
+
+//String 
+
+var texto = "Curso de JavaScript";
+
+//number 
+var numeroInteiro = -7;
+var numeroFracionado = 111210.75
+
+//boolean
+var teste = true 
+var teste = false
+
+//alert(numeroFracionado) //abrir um dialog
+//document.write(texto)
+//alert(texto)
+console.log(texto);
+
+*/
+/*AULA 153
+var nome = prompt("Digitre o seu nome");
+var idade = prompt("Digite a sua idade");
+document.write('<h1> Olá '+nome,' tudo bem? Estou vendo aqui que você possui '+idade,' anos </h1>');
+*/
+
+/*AULA 155
+//null
+var teste = null;
+
+var teste2;
+
+console.log(teste);
+console.log(teste2);
+
+//undefined 
+*/
+//AULA 161
+/*
+var nota = prompt("Digite a nota do aluno:");
+var media = 7;
+
+if (nota >= media) {
+    document.write("Você foi aprovado");
+}
+else {
+    document.write("Você está reprovado");
+}
+*/
+
+//AULA 162
+/*
+var variavel1 = 10;
+var variavel2 = 20;
+
+//variavel1 = parseInt(variavel1);
+//variavel2 = parseFloat(variavel2);
+
+console.log(variavel1);
+console.log(variavel2);
+
+document.write(variavel1.toString() + variavel2.toString());
+*/
+//AULA 164
+
+//true
+
+var nome = prompt("Digite o seu nome");
+var altura = prompt("Digite a sua altura em Centimetros");
+var peso = prompt("Digite o seu peso");
+peso = parseFloat(peso);
+altura = parseFloat(altura);
+var M= peso / (altura *altura);
+console.log(M);
+
+
+
+
